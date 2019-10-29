1541385ea01e7e1a46187027493cf43989c6cc49
Vio <vio@beanon.com>
2019-10-29 19:26:47 +0100

    Initial files

---
+++ README.md
@@ -0,0 +1,9 @@
+# BundleStats config example for custom webpack config
+
+```shell
+npm install
+```
+
+```shell
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const CONTEXT = path.join(__dirname, "src");
+const DIST = path.join(__dirname, "dist");
+
+module.exports = (_, { mode }) => ({
+	context: CONTEXT,
+	entry: {
+		app: "./index.jsx"
+	},
+	output: {
+		path: DIST,
+		filename: mode === "production" ? "[name].[contenthash:8].js" : "[name].js"
+	},
+	resolve: {
+		extensions: [".js", ".jsx", ".json"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				loader: "babel-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							hmr: mode === "production"
+						}
+					},
+					"css-loader"
+				]
+			},
+			{
+				test: /\.(jpg|png|gif|webp|svg)$/,
+				loader: "file-loader",
+				options: {
+					name:
+						mode === "production"
+							? "[path][name].[contenthash:8].[ext]"
+							: "[path][name].[ext]"
+				}
+			}
+		]
+	},
+	plugins: [
+		new HtmlPlugin({
+			template: "index.html",
+			filename: "index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename:
+				mode === "production" ? "[name].[contenthash:8].css" : "[name].css"
+		})
+	]
+});
